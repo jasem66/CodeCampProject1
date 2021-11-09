9b6a94a0503ab24a1f98ce49cbfaa5a88738f5eb
jasem66 <jasemsarafraz66@gmail.com>
2021-11-09 11:11:01 +0200

    Update app.js

--- sideBar/app.js
+++ sideBar/app.js
@@ -4,12 +4,9 @@ const sideBar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function(){
    
-     if(sideBar.classList.contains("show-sidebar")){
-         sideBar.classList.remove("show-sidebar");
-        
-     }
-     else {
-        sideBar.classList.add("show-sidebar");
-
-     }
+  sideBar.classList.toggle("show-sidebar")
 });
+
+closeBtn.addEventListener("click", function(){
+sideBar.classList.remove("show-sidebar")
+})
